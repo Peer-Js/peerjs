ae5988645a2818a57915016100ca9a73a5f320a7
peterkariuki-01 <pjeykariuki2003@gmail.com>
2024-09-05 01:50:05 +0300

    added why us

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ const navList = document.querySelector('.nav-list');
 menuIcon.addEventListener('click', () => {
     navList.style.display = navList.style.display === 'flex' ? 'none' : 'flex';
 });
+
+//why us section javascript code
+
+document.addEventListener('DOMContentLoaded', function() {
+    const readMoreIcons = document.querySelectorAll('.read-more-icon');
+    
+    readMoreIcons.forEach(readMoreIcon => {
+        readMoreIcon.addEventListener('click', function() {
+            const subSectionContainer = readMoreIcon.closest('.sub-section-container');
+            const moreDetailsContainer = subSectionContainer.querySelector('.more-details-container');
+            const iconImage = readMoreIcon.querySelector('.icon-image');
+            
+          
+            subSectionContainer.classList.toggle('expanded');
+            
+            
+           (subSectionContainer.classList.contains('expanded')) ? iconImage.src = 'images/angle-down.svg'
+           : iconImage.src = 'images/enlarge.svg';
+                
+        });
+        });
+        });
+
+
+
+
+
+
